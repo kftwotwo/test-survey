b0fc91569efee7a5a561ed9692869a871f2703e8
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-21 14:53:37 -0700

    add the function to choose what class you should you take

--- js/scripts.js
+++ js/scripts.js
@@ -1,61 +1,33 @@
-var ruby,rails
-var css
-var andriod
+var rubyScore
+var cssScore
 
 $(document).ready(function() {
-  var course1 = $("input:radio[name=color]:checked").val();
-  var course2 = $("input:radio[name=color]:checked").val();
-  var course3 = $("input:radio[name=color]:checked").val();
-  var course4 = $("input:radio[name=color]:checked").val();
-  var course5 = $("input:radio[name=color]:checked").val();
-    $("#survey-lang").submit(function(event) {
-      var userName = $("#userName").val();
-      var userAddress = $("#userAddress").val()
-      $(".output-name").text(userName);
-      $(".output-address").text(userAddress);
+  $("#survey-lang").submit(function(event) {
+    var course = $("input:radio[name=course]:checked").val();
+    var course2 = $("input:radio[name=course2]:checked").val();
+    var course3 = $("input:radio[name=course3]:checked").val();
+    var course4 = $("input:radio[name=course4]:checked").val();
+    var course5 = $("input:radio[name=course5]:checked").val();
+    var userName = $("#userName").val();
+    var userAddress = $("#userAddress").val();
+    $(".output-name").text(userName);
+    $(".output-address").text(userAddress);
+    rubyScore = 0;
+    cssScore = 0;
 
-      if(course1 == "ruby") {
-      ruby,rail = ruby,rails + 1;
-      } else if (course1 = "css") {
-      css = css + 1;
-      } else if (course1 = "andriod") {
-        andriod = andriod + 1;
+      if(course === "ruby") {
+        rubyScore = rubyScore + 1;
+      } else {
+        cssScore = cssScore + 1;
       }
 
-
-      if(course2 == "ruby") {
-      ruby,rail = ruby,rails + 1;
-      } else if (course2 = "css") {
-      css = css + 1;
-    } else if (course2 = "andriod") {
-        andriod = andriod + 1;
+      if (rubyScore > cssScore) {
+        $(".par").fadeIn("slow");
+        $(".result").prepend("Ruby!")
+      } else if (rubyScore < cssScore){
+        $(".par").fadeIn("slow");
+        $(".result").text("CSS/Desgin")
       }
-
-      if(course3 == "ruby") {
-      ruby,rail = ruby,rails + 1;
-      } else if (course3 = "css") {
-      css = css + 1;
-      } else if (course3 = "andriod") {
-        andriod = andriod + 1;
-      }
-
-      if(course4 == "ruby") {
-      ruby,rail = ruby,rails + 1;
-      } else if (course4 = "css") {
-      css = css + 1;
-      } else if (course4 = "andriod") {
-        andriod = andriod + 1;
-      }
-
-      if(course5 == "ruby") {
-      ruby,rail = ruby,rails + 1;
-      } else if (course5 = "css") {
-      css = css + 1;
-      } else if (course5 = "andriod") {
-        andriod = andriod + 1;
-      };
-
-
     event.preventDefault();
   });
 });
