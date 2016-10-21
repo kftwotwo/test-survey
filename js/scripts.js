da04686a2970d656c866f77778eb66fac54a55d8
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-21 10:01:22 -0700

    add user input in the name and address

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,11 @@ $(document).ready(function() {
   var course3 = $("input:radio[name=color]:checked").val();
   var course4 = $("input:radio[name=color]:checked").val();
   var course5 = $("input:radio[name=color]:checked").val();
+    $("#survey-lang").submit(function(event) {
+      var userName = $("#userName").val();
+      var userAddress = $("#userAddress").val()
+      $(".output-name").text(userName);
+      $(".output-address").text(userAddress);
+    event.preventDefault();
+  });
 });
