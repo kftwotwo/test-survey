fddc64534bf9562efda32ed1960d5a69b7e9c4d9
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-21 10:38:36 -0700

    add rest of radio values

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,49 @@ $(document).ready(function() {
       var userAddress = $("#userAddress").val()
       $(".output-name").text(userName);
       $(".output-address").text(userAddress);
+
+      if(course1 == "ruby") {
+      ruby,rail = ruby,rails + 1;
+      } else if (course1 = "css") {
+      css = css + 1;
+      } else if (course1 = "andriod") {
+        andriod = andriod + 1;
+      }
+
+
+      if(course2 == "ruby") {
+      ruby,rail = ruby,rails + 1;
+      } else if (course2 = "css") {
+      css = css + 1;
+    } else if (course2 = "andriod") {
+        andriod = andriod + 1;
+      }
+
+      if(course3 == "ruby") {
+      ruby,rail = ruby,rails + 1;
+      } else if (course3 = "css") {
+      css = css + 1;
+      } else if (course3 = "andriod") {
+        andriod = andriod + 1;
+      }
+
+      if(course4 == "ruby") {
+      ruby,rail = ruby,rails + 1;
+      } else if (course4 = "css") {
+      css = css + 1;
+      } else if (course4 = "andriod") {
+        andriod = andriod + 1;
+      }
+
+      if(course5 == "ruby") {
+      ruby,rail = ruby,rails + 1;
+      } else if (course5 = "css") {
+      css = css + 1;
+      } else if (course5 = "andriod") {
+        andriod = andriod + 1;
+      };
+
+
     event.preventDefault();
   });
 });
