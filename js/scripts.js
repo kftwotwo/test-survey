cf4108ac2ef8e95bfde74b218c91800cb85227cc
Kevin A. Finley <kf.two.two@gmail.com>
2016-10-21 09:27:05 -0700

    add basic js and jquery

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+var ruby,rails
+var css
+var andriod
+
+$(document).ready(function() {
+  var course1 = $("input:radio[name=color]:checked").val();
+  var course2 = $("input:radio[name=color]:checked").val();
+  var course3 = $("input:radio[name=color]:checked").val();
+  var course4 = $("input:radio[name=color]:checked").val();
+  var course5 = $("input:radio[name=color]:checked").val();
+});
